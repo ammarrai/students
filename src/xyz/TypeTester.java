386d45c4e37bcf8e68e4df050c5c571e856e3d64
Mark Rai <markraidc@gmail.com>
2017-06-18 03:15:17 -0400

    addd TypeTester class
    The typeTester() will allow us to determine the  datatype of variables.

---
+++ src/xyz/TypeTester.java
@@ -0,0 +1,43 @@
+package xyz;
+
+public class TypeTester {
+
+	public void printType(byte input) {
+		System.out.println("byte");
+	}
+
+	public void printType(short input) {
+		System.out.println("short");
+	}
+
+	public void printType(int input) {
+		System.out.println("int");
+	}
+
+	public void printType(long input) {
+		System.out.println("long");
+	}
+
+	public void printType(float input) {
+		System.out.println("float");
+	}
+
+	public void printType(double input) {
+		System.out.println("double");
+	}
+
+	public void printType(char input) {
+		System.out.println("char");
+	}
+
+	public void printType(boolean input) {
+		System.out.println("boolean");
+	}
+
+	public void printType(String input) {
+		System.out.println("String");
+	}
+	
+	
+	
+}
